1c57fb690307d51b01b251f9068d72eb84b999cb
Yakup eren ermurat <yakuperenermurat@gmail.com>
2024-04-17 12:06:39 +0300

    Variables degiskenler.

---
+++ java-002-variables/src/main/java/com/yakuperenermurat/Car.java
@@ -0,0 +1,47 @@
+package com.yakuperenermurat;
+
+public class Car {
+
+    public static void main(String[] args) {
+        //sayi teker
+        int teker = 5;
+
+
+        //sayi direksiyon
+        int direksiyon = 1;
+
+
+        //sayi motor gücü
+        int motorGucu = 5000;
+
+        //yazı motor türü
+        String motorTuru = "Japon";
+
+        //yazı marka
+        String marka = "Mercedes";
+
+        // sayi model yılı
+        int modelYili = 2024;
+
+        //yazı renk
+        String renk = "Lacivert";
+
+        //var yok plaka
+        boolean plaka = true;
+
+        //sayi kapi adeti
+        int kapiAdeti = 5;
+
+        System.out.println("Araba sınıfı merhaba.");
+        System.out.println("Teker: " + teker);
+        System.out.println("Direksiyon:" + direksiyon);
+        System.out.println("MotorGucu: " + motorGucu);
+        System.out.println("MotorTuru: " + motorTuru);
+        System.out.println("Marka: " + marka);
+        System.out.println("ModelYili: " + modelYili);
+        System.out.println("Renk: " + renk);
+        System.out.println("Plaka: " + plaka);
+        System.out.println("KapiAdeti: " + kapiAdeti);
+
+    }
+}
