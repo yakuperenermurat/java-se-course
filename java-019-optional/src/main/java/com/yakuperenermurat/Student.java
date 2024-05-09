8ab19e818f4c27f1496a2ccbaa45dc0c0ec82171
yakuperenermurat <yakuperenermurat@gmail.com>
2024-05-09 09:30:26 +0300

    optional project

---
+++ java-019-optional/src/main/java/com/yakuperenermurat/Student.java
@@ -0,0 +1,44 @@
+package com.yakuperenermurat;
+// POJO veya Modelde denebilir
+public class Student {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Student(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
